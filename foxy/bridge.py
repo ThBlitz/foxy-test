eb99bb9859d76d3b3284e5b6dc6caece9749ea29
Preetham Rakshith <61616034+ThBlitz@users.noreply.github.com>
2022-07-09 19:16:49 +0530

    foxy

--- foxy/bridge.py
+++ foxy/bridge.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import stdOut
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
@@ -8,23 +9,38 @@ if __name__ == '__main__':
     command = str(sys.argv[1])
     arg_2 = str(sys.argv[2])
     arg_3 = str(sys.argv[3])
+    virtual_env_var = str(sys.argv[4])
+    envs_path = str(sys.argv[5])
 
-    list_of_commands = [
-        'info',
-        'commands',
-        'lsit_envs',
-        'env_info',
-        'create',
-        'remove',
-        'clone',
-        'rename',
-        'list_versions',
-        'clone_version',
-        'change_version',
-        'settings'
-    ]
+    permissions = ('o', 'i', 'a') # inside, outside, anywhere
+
+    list_of_commands = {
+        'info': ('a'),
+        'commands': ('a'),
+        'list_envs': ('a'),
+        'env_info': ('i', 'o'),
+        'create': ('o'),
+        'remove': ('o'),
+        'clone': ('o'),
+        'rename': ('o'),
+        'list_versions': ('o', 'i'),
+        'clone_version': ('o', 'i'),
+        'change_version': ('o', 'i'),
+        'settings': ('i', 'o')
+    }
     
-    all_commands = set(list_of_commands)
+    permission = []
+    permission.append('a')
+    if virtual_env_var == "__VIRTUAL_ENV_VARIABLE_NOT_DEFINED__":
+        virtual_env_var = None 
+        permission.append('o')
+    else:
+        permission.append('i')
+
+    if command not in list_of_commands:
+        stdOut.print_error(0)
+
+    else:
 
     
         
--- foxy/stdOut.py
+++ foxy/stdOut.py
@@ -1,5 +1,26 @@
 
 
 
+error_messages = {
+    0: [
+        'invalid command',
+        'please enter ~ $fox OR $fox commands',
+        'for more information'
+    ]
+}
+
+def print_messg(messg):
+    for line in messg:
+        print(line)
+    return
+
+def format_messg(messg):
+    
+    return messg
+
+def print_error(error_code):
+    print_messg(error_messages[error_code])
+    return
+
 def print_(print_type):
     return 
